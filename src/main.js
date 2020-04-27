f85a73da5ddbc07bf97a96d4007fb32aa21b6aec
Evellyn <xe.uyemura@gmail.com>
2020-04-27 12:56:47 -0300

    Layout

--- src/main.js
+++ src/main.js
@@ -1,33 +1,24 @@
 import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 
-data.pokemon.forEach(element => {
+//Pegando a id "Root" do HTML
+const content = document.querySelector("#root") 
+
+data.pokemon.forEach(element => {   
     
-    //Criando uma div no HTML  "<div></div>"
-    let createDiv = document.createElement("div") 
-    //Criando um elemento de imagem. "<img src=''>"
-    let image = document.createElement("img") 
+    let createDiv = 
+    `
+        <div class="box-pokemon ${element.type[0]}-light"> 
+            <div class="info-pokemon">
+                <p>#${element.num} ${element.name}</p>
+            </div>
+            <div class="box-image ${element.type[0]}">
+                <img src="${element.img}">  
+            </div>
+        </div>
+    `
 
-    //Pegando url do "data" e adicionando. "<img src='url da imagem'>
-    image.src = element.img 
-    //Colocando o nome dos Pokemons na variável.
-    let namePokemon = document.createTextNode(element.name) 
-
-    //Colocando a classe "Box Pokemon" nas divs que serão criadas. <div class="box-pokemon"></div>
-    createDiv.classList.add("box-pokemon") 
-
-    //Colocando o nome do Pokemon dentro da div. "<div>Pikachu</div>"
-    createDiv.appendChild(namePokemon) 
-    //Colocando a imagem do Pokemon na div. <div><img src='url da imagem'>Pikachu</div>
-    createDiv.appendChild(image) 
-
-    //Pegando a id "Root" do HTML
-    let content = document.querySelector("#root") 
     //Adicionando as divs dentro da div "Root".
-    content.appendChild(createDiv) 
+    content.innerHTML += createDiv
 });
-
-
-
-console.log(data.pokemon[0].name);
 
