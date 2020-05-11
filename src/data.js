0720102200b7295080d232cd56a14a859b87d008
Evellyn <xe.uyemura@gmail.com>
2020-05-11 15:05:53 -0300

    adicionando modal

--- src/data.js
+++ src/data.js
@@ -1,4 +1,5 @@
 // Funções de exemplo
+// import pokemon from "./data/pokemon/pokemon";
 
 export const filterType = (bttnFilter, pokemons) => {
   let type = bttnFilter[bttnFilter.selectedIndex].value
@@ -16,7 +17,30 @@ export const filterType = (bttnFilter, pokemons) => {
 return emptyArray
 };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+export const filterOrder = (bttnOrder, pokemons) => {
+  let order = bttnOrder[bttnOrder.selectedIndex].value
+  
+  if(order == "a-z"){
+    return pokemons.sort(function (a, b) {
+      if (a.name > b.name) {
+        return 1;
+      }
+      if (a.name < b.name) {
+        return -1;
+      }
+      return 0;
 
+    });
+  } else {
+      return pokemons.sort(function (a, b) {
+        if (a.name > b.name) {
+          return -1;
+        }
+        if (a.name < b.name) {
+          return 1;
+        }
+        return 0;
+
+    });
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,28 +1,129 @@
-import { filterType } from './data.js';
+import { filterType, filterOrder } from './data.js';
 import data from './data/pokemon/pokemon.js';
 
-//Pegando a id "Root" do HTML
-const content = document.querySelector("#root") 
-const bttnFilter = document.querySelector("#filter-by-type")
+const content = document.querySelector("#root"); 
+const bttnFilter = document.querySelector("#filter-by-type");
+const bttnOrder = document.querySelector("#order-button");
+
+const modal = document.querySelector("#modals");
+const span = document.getElementsByClassName("close")[0];
+
+const namePokemon = document.querySelector(".modal-name h1");
+const heightPokemon = document.querySelector("#height");
+const weigthPokemon = document.querySelector("#weight");
+const candyPokemon = document.querySelector("#candy");
+const candyQuantityPokemon = document.querySelector("#candy-quantity");
+const typePokemon = document.querySelector("#types");
+const weaknessPokemon = document.querySelector("#weakness");
+const evolutionPokemon = document.querySelector(".content-evolution");
 
 function showElements(data){
-    data.forEach(element => {   
-    
-        let createDiv = 
-        `
-            <div class="box-pokemon ${element.type[0]}-light"> 
-                <div class="info-pokemon">
-                    <p>#${element.num} ${element.name}</p>
+    if(data.length != 0){
+        data.forEach(element => {   
+        
+            let createDiv = 
+            `
+                <div class="box-pokemon ${element.type[0]}-light" data-num="${element.num}"> 
+                    <div class="info-pokemon" data-num="${element.num}">
+                        <p data-num="${element.num}">#${element.num} ${element.name}</p>
+                    </div>
+                    <div class="box-image ${element.type[0]}" data-num="${element.num}">
+                        <img src="${element.img}" data-num="${element.num}">  
+                    </div>
                 </div>
-                <div class="box-image ${element.type[0]}">
-                    <img src="${element.img}">  
-                </div>
-            </div>
+            `
+        
+            //Adicionando as divs dentro da div "Root".
+            content.innerHTML += createDiv
+        });
+
+        let cards = document.querySelectorAll(".box-pokemon")
+        addEventCards(cards)
+
+    } else {
+        let createDiv =
         `
-    
-        //Adicionando as divs dentro da div "Root".
+            <p class="alert">Pokemons não encontrados</p>
+        `
         content.innerHTML += createDiv
-    });
+    }
+}
+
+function addEventCards(pokemon) {
+    pokemon.forEach(card => {
+        card.addEventListener("click", function(event) {
+            //O elemento que foi clicado.
+            let targetElement = event.target || event.srcElement;
+            //Guarda o num do elemento.
+            let num = targetElement.dataset.num
+            //Caso o número seja igual ao num, guarda na variável.
+            let pokemonClicked = data.pokemon.find(element =>  element.num == num)
+
+            namePokemon.innerHTML = `#${pokemonClicked.num} ${pokemonClicked.name}`
+            heightPokemon.innerHTML = `${pokemonClicked.height}`
+            weigthPokemon.innerHTML = `${pokemonClicked.weight}`
+            candyPokemon.innerHTML = `${pokemonClicked.candy}`
+            candyQuantityPokemon.innerHTML = `${pokemonClicked.candy_count}`
+
+            typePokemon.innerHTML = ""
+            pokemonClicked.type.forEach(element => {
+                typePokemon.innerHTML += `<div class="chip ${element}">${element}</div>`
+            })
+
+            weaknessPokemon.innerHTML = ""
+            pokemonClicked.weaknesses.forEach(element => {
+                weaknessPokemon.innerHTML += `<div class="chip ${element}">${element}</div>`
+            })
+
+            evolutionPokemon.innerHTML = ""
+            // evolutionPokemon.innerHTML += `
+            // <div class="evolution-info"> 
+            //     <img src="${pokemonClicked.img}">
+            //     <p>${pokemonClicked.num}</p>
+            //     <p>${pokemonClicked.name}</p>
+            // </div>
+            // `
+            
+            if(pokemonClicked.prev_evolution != undefined){
+                pokemonClicked.prev_evolution.forEach(element => {
+                    let pokemonData = data.pokemon.find(pokemon => pokemon.num == element.num)
+                    evolutionPokemon.innerHTML += `
+                    <div class="evolution-info"> 
+                        <img src="${pokemonData.img}">
+                        <p>#${pokemonData.num}</p>
+                        <p>${pokemonData.name}</p>
+                    </div>
+                    `
+                })
+            }
+
+            if(pokemonClicked.next_evolution != undefined){
+                pokemonClicked.next_evolution.forEach(element => {
+                    let pokemonData = data.pokemon.find(pokemon => pokemon.num == element.num)
+                    evolutionPokemon.innerHTML += `
+                    <div class="evolution-info"> 
+                        <img src="${pokemonData.img}">
+                        <p>#${pokemonData.num}</p>
+                        <p>${pokemonData.name}</p>
+                    </div>
+                    `
+                })
+            }
+
+            //Quando clicar nas caixinhas, aparece o modal.
+            modal.style.display = "block"
+            //Esconder o modal quando for clicado no "x"
+            span.onclick = function() {
+                modal.style.display = "none";
+            }
+            //Caso o clique seja fora do modal, ele desaparece.
+            window.onclick = function(event) {
+                if(event.target == modal) {
+                    modal.style.display = "none"
+                }
+            }
+        })
+    })
 }
 
 function clearHtml(){
@@ -37,6 +138,11 @@ bttnFilter.addEventListener("change", function() {
     showElements(filterTypePokemon)
 })
 
+bttnOrder.addEventListener("change", function() {
+    let order = filterOrder(bttnOrder, data.pokemon)
+    clearHtml()
+    showElements(order)
+})
 
 
 
